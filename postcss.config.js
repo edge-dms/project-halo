80268cf81cf166a9069563026e2ff9f30b740a75
Kevin Connor <kevin@kitchenkevin.com>
2026-02-16 11:08:27 -0500

    Fix: Removed git conflict markers from config files

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,6 @@
-import tailwindcss from '@tailwindcss/postcss';
-import autoprefixer from 'autoprefixer';
-
 export default {
-  plugins: [tailwindcss(), autoprefixer()],
-<<<<<<< HEAD
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
-=======
-};
->>>>>>> cf6a11354327146b76711d19a72789f30b1f89a3
