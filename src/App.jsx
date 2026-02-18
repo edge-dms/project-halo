579e2291f7336d7fe15405f0ed31468f9f983644
Edge Digital Marketing Systems <kevin.connor@edgebrandingsolutions.com>
2026-02-17 23:32:12 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -119,7 +119,7 @@ function Dashboard({ onLogout }) {
 
   // --- API ACTIONS ---
   const geocodeAddress = async (address) => {
-    const url = `https://services.leadconnectorhq.com/hooks/hXcSSA35KVSLC2674wFT/webhook-trigger/73268709-d1cf-457d-9792-41383bb6f799&limit=1`;
+    const url = `https://services.leadconnectorhq.com/hooks/hXcSSA35KVSLC2674wFT/webhook-trigger/e3e37ad7-856d-4a3f-9918-c974d07648df`;
     const resp = await fetch(url);
     const data = await resp.json();
     return data.features?.length > 0 ? { lat: data.features[0].center[1], lng: data.features[0].center[0] } : null;
